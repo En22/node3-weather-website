e52deafb8a750d7f8280cef27d801901f0ec9628
Enrico Medda <enricomedda@Enricos-MBP.lan>
2022-01-07 12:40:34 +0100

    Init commit

---
+++ src/utils/forescast.js
@@ -0,0 +1,16 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=1632818c52a1e3ce77b8ddf55336e840&query='+ latitude +','+ longitude +'&units=m'
+    request({ url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather services!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. Try another search', undefined)
+        } else {
+            callback( undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It fells like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
