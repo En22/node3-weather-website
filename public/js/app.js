c4369ffef47fd613e8eace382c089c8fd1f36e2e
Enrico Medda <enricomedda@Enricos-MBP.lan>
2022-01-07 15:34:27 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-// messageOne.textContent = 'From js'
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
@@ -12,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             console.log(data.error)
